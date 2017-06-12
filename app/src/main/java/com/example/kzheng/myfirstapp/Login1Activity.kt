2960a633b88c769cc50efb06800be4e7f4550c05
Ken Zheng <kzheng@ucdavis.edu>
2017-06-12 14:28:50 -0700

    can now retrieve account and password text

--- app/src/main/java/com/example/kzheng/myfirstapp/Login1Activity.kt
+++ app/src/main/java/com/example/kzheng/myfirstapp/Login1Activity.kt
@@ -2,11 +2,31 @@ package com.example.kzheng.myfirstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 
 class Login1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login1)
+
+        val loginButton = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener { login(it) }
+    }
+
+    public fun login(view: View) {
+        val accountEditText: EditText? = findViewById(R.id.accountEditText) as? EditText
+        val passwordEditText: EditText? = findViewById(R.id.passwordEditText) as? EditText
+
+        val account = accountEditText?.text.toString()
+        val password = passwordEditText?.text.toString()
+
+        if (account.length > 0 && password.length > 0) {
+            Log.i("login", "logging in with account: $account password: $password")
+        } else {
+            Log.i("login", "invalid account and password")
+        }
     }
 }
