6dd7ab5a53c8abfad363ab6222acdf5732247f55
Ken Zheng <kzheng@ucdavis.edu>
2017-06-12 13:28:45 -0700

    Added Login1Activity

--- app/src/main/java/com/example/kzheng/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/kzheng/myfirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kzheng.myfirstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     public fun login(view: View) {
-        
+        // change intent to your class to switch activities
+        val intent = Intent(this, Login1Activity::class.java)
+        startActivity(intent)
     }
 }
