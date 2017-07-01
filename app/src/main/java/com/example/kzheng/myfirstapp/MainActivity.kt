90eb853870b3eca3427dc92c0e6e35e1603198a5
Ken Zheng <kzheng@ucdavis.edu>
2017-07-01 14:59:26 -0700

    Added MainActivity. MainActivity is now the entry activity for all our activities

---
+++ app/src/main/java/com/example/kzheng/myfirstapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.kzheng.myfirstapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val kenButton = findViewById(R.id.kenButton)
+        kenButton.setOnClickListener { kenButtonClicked(it) }
+
+        val garthButton = findViewById(R.id.garthButton)
+        garthButton.setOnClickListener { garthButtonClicked(it) }
+
+        val benButton = findViewById(R.id.benButton)
+        benButton.setOnClickListener { benButtonClicked(it) }
+    }
+
+    fun kenButtonClicked(view: View) {
+        // TODO: start your intent here
+//        val intent = Intent(this, LoginActivityBen::class.java)
+//        startActivity(intent)
+    }
+
+    fun garthButtonClicked(view: View) {
+        // TODO: start your intent here
+//        val intent = Intent(this, LoginActivityBen::class.java)
+//        startActivity(intent)
+    }
+
+    fun benButtonClicked(view: View) {
+        // TODO: start your intent here
+//        val intent = Intent(this, LoginActivityBen::class.java)
+//        startActivity(intent)
+    }
+}
